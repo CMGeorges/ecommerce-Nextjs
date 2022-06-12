491de782037d99950fa36d75014da8f4cfee6557
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2022-06-12 13:43:33 -0400

    setup the project

---
+++ sanity_ecommerce/schemas/product.js
@@ -0,0 +1,45 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            type: 'Image',
+            type: 'array',
+            of: [
+                {
+                    type: 'image'
+                }
+            ],
+            options: {
+                hotspot: true
+
+            }
+        },
+        {
+            name:'name',
+            type: 'string',
+            title: 'Name',
+        },
+        {
+            name:'slug',
+            type: 'slug',
+            title: 'Slug',
+            options: {
+                source: 'name',
+                maxLength: 96
+            }
+        },
+        {
+            name:'price',
+            type: 'number',
+            title: 'Price',
+        },
+        {
+            name:'details',
+            type: 'string',
+            title: 'Details',
+        },
+    ]
+}

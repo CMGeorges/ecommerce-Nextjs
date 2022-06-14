27f6fd5bf68d19e0fe535fdf0ed80a471c8829e4
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2022-06-13 23:34:16 -0400

    Add Layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { AiFillInstagram,AiOutlineTwitter,AiFillFacebook } from 'react-icons/ai'
+import { FaSnapchatGhost } from 'react-icons/fa'
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2022 All rights reserved © CMGeorges&Cie </p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+        <FaSnapchatGhost />
+      </p>
+    </div>
   )
 }
 
